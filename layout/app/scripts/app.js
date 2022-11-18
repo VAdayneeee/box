15ba554e9bf868b6547a0dc29c9f1487bb2be68f
Anh Vũ <you@example.com>
2022-11-18 08:57:47 +0700

    first-commit

---
+++ layout/app/scripts/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".box__layout").slick({
+        slidestoShow: 4,
+        slidetoScroll: 2,
+        infinite: false,
+        arrows: false,
+        // autoplay: true,
+        // autoplaySpeed: 2000, 
+    });
+}) ;
