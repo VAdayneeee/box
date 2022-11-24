ad1fcf63152742e8eb99138593ba1397b940b7bd
Anh Vũ <you@example.com>
2022-11-24 11:54:12 +0700

    first-commit

--- layout/app/scripts/app.js
+++ layout/app/scripts/app.js
@@ -1,13 +1,32 @@
-const testString = 'This is a test!';
+// repeat in js
 
-console.log(testString);
+function multiplyNode(node, count, deep) {
+    for (var i = 0, copy; i < count - 1; i++) {
+        copy = node.cloneNode(deep);
+        node.parentNode.insertBefore(copy, node);
+    }
+}
 
-const bullets = [
-  'radio1',
-  'radio2',
-  'radio3',
-];
+multiplyNode(document.querySelector('.box__layout--content'), 10, true);
 
-bullets.map((bullet) => {
-  console.log(bullet);
-});
+// button click
+
+var box__layout = document.querySelector('.box__layout--content');
+var text = ['.box__layout--content'];
+var i = 0;
+
+function prev() {
+    if(i <= 0) i = text.length;	
+	i--;
+	return setImg();
+}
+
+function next() {
+    if(i <= 0) i = text.length;	
+	i--;
+	return setImg();
+}
+
+function setImg(){
+    return box__layout.setAttribute('.box__layout--content');
+}
